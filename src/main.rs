eff10b911a321f3cbd3f960499620369a566d62a
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2024-04-07 18:44:07 +0100

    created initial structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Data {
+    value: String,
+}
+
+struct Ingredient {
+    data: Data,
+    size: u32,
+    depth: u32,
+}
+
+struct Soup {
+    data: Vec<Ingredient>,
+}
